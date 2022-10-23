f26f5b41e74ce85283c49f4362965d1a96751f6d
Athos Boudourakis <a.boudourakis@outlook.com>
2022-10-24 00:58:47 +0300

    [WIP]feature(*): add services - tidy up code

--- Services/weather.py
+++ Services/weather.py
@@ -1,41 +1,21 @@
-import csv
-import re
 from csv import reader
 import sys
 from urllib import request
 from urllib import error
 import pandas as pd
-import streamlit as st
-import numpy as np
 
 
-def getWeather():
+def get_weather():
     try:
         ResultBytes = request.urlopen(
             "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Limassol?unitGroup"
             "=metric&include=days&key=ACRB86QG8FL6QRU43EGJ26W8V&contentType=csv").read().decode('UTF-8')
-        print('ResultBytes', ResultBytes)
-        # Parse the results as CSV
-        # CSVText = csv.reader(codecs.iterdecode(ResultBytes, 'utf-8'))
-        # Parse the results as JSON
-        # jsonData = json.loads(ResultBytes.decode('utf-8'))
-        # TODO map data onto the table below
-        # print('ResultBytes[0]', ResultBytes[1])
-        # results = []
-        # with ResultBytes as csvfile:
-        #     reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
-        #     for row in reader:  # each row is a list
-        #         results.append(row)
-        #
-        # print('results', results)
+        # print('ResultBytes', ResultBytes)
         array = ResultBytes.splitlines()
         tableData = []
         for line in reader(array):
             tableData.append(line)
-            # print('line', line)
-        # print('tableData', tableData)
 
-        # dfColumns = [tableData[0][0], 'Time', tableData[0][4]]
         dfColumns = ['Περιοχή', 'Ημερομηνία', 'Θερμοκρασία', 'Μικρότερη', 'Μεγαλύτερη']
         dfRows = []
         for lineIndex in range(1, len(tableData)):
@@ -44,13 +24,7 @@ def getWeather():
                            tableData[lineIndex][4],
                            tableData[lineIndex][3],
                            tableData[lineIndex][2]])
-        # print('dfColumns', dfColumns)
-        # print('dfRows', dfRows)
         return pd.DataFrame(data=dfRows, columns=dfColumns)
-
-        # # Display the dataframe and allow the user to stretch the dataframe
-        # # across the full width of the container, based on the checkbox value
-        # st.dataframe(df, use_container_width=st.session_state.use_container_width)
 
     except error.HTTPError as e:
         ErrorInfo = e.read().decode()
--- main.py
+++ main.py
@@ -1,32 +1,26 @@
+from Services.weather import get_weather
+
 import streamlit as st
 
 import sys
+
 # import os
 
-sys.path.append('./Services')
+# sys.path.append('./Services')
 
-from Services.weather import getWeather
+weatherData = get_weather()
+# st.dataframe(weatherData)
 
-weatherData = getWeather()
-# print('weatherData', weatherData)
-st.dataframe(weatherData, use_container_width=st.session_state.use_container_width)
-# TODO refactor to return data and use them in a table
-# st.checkbox("Use container width", value=False, key="use_container_width")
-# df = load_data()
-# # Display the dataframe and allow the user to stretch the dataframe
-# # across the full width of the container, based on the checkbox value
-# st.dataframe(df, use_container_width=st.session_state.use_container_width)
+col1, col2, col3 = st.columns(3)
 
-# col1, col2, col3 = st.columns(3)
-#
-# with col1:
-#     st.header("A cat")
-#     st.image("https://static.streamlit.io/examples/cat.jpg")
-#
-# with col2:
-#     st.header("A dog")
-#     st.image("https://static.streamlit.io/examples/dog.jpg")
-#
-# with col3:
-#     st.header("An owl")
-#     st.image("https://static.streamlit.io/examples/owl.jpg")
+with col1:
+    st.header("Weather in Limassol")
+    st.dataframe(weatherData)
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
