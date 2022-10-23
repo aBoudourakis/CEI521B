9d6755ccee4b4e0b0ac5ec0da031bdb1aada7398
Athos Boudourakis <a.boudourakis@outlook.com>
2022-10-23 21:45:03 +0300

    [WIP]feature(*): add services - add TODO and indication to content

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ sys.path.append('./Services')
 from Services.weather import getWeather
 
 getWeather()
+# TODO refactor to return data and use them in a table
+# st.checkbox("Use container width", value=False, key="use_container_width")
+# df = load_data()
+# # Display the dataframe and allow the user to stretch the dataframe
+# # across the full width of the container, based on the checkbox value
+# st.dataframe(df, use_container_width=st.session_state.use_container_width)
 
 # col1, col2, col3 = st.columns(3)
 #
