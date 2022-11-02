78be899fd4b6a00d82729310fda96a24028e8696
Athos Boudourakis <a.boudourakis@outlook.com>
2022-11-02 22:34:49 +0200

    [WIP]feature(*): add services - change column names to greek - change api uri to limit 20 entries - comment out prints

--- Services/crypto.py
+++ Services/crypto.py
@@ -9,35 +9,35 @@ import pandas as pd
 def get_crypto():
     try:
         result_bytes = request.urlopen(
-            "https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=EUR").read().decode('UTF-8')
+            "https://api.coinstats.app/public/v1/coins?skip=0&limit=20&currency=EUR").read().decode('UTF-8')
         # print('result_bytes', result_bytes)
         array = result_bytes.splitlines()
-        print('array', array)
-        print('array[0]', array[0])
+        # print('array', array)
+        # print('array[0]', array[0])
         dictionary = json.loads(array[0])
-        print('dictionary', dictionary['coins'])
-        print('type dictionary', type(dictionary['coins']))
+        # print('dictionary', dictionary['coins'])
+        # print('type dictionary', type(dictionary['coins']))
 
         coins_array = json.loads(array[0])
         pd_df = pd.DataFrame.from_dict(coins_array)
         coins_from_pd_df = pd_df['coins']
         expanded_coins = (coins_from_pd_df.explode().tolist())
-        print('expanded_coins', expanded_coins)
+        # print('expanded_coins', expanded_coins)
 
         data_frame = pd.DataFrame.from_dict(dictionary)
-        print('data_frame type', type(data_frame))
-        print('data_frame', data_frame)
+        # print('data_frame type', type(data_frame))
+        # print('data_frame', data_frame)
 
-        print('test locate row', data_frame.loc[0])
-        print('test locate row type', type(data_frame.loc[0]))
+        # print('test locate row', data_frame.loc[0])
+        # print('test locate row type', type(data_frame.loc[0]))
 
         df_from_df = pd.DataFrame.from_records(coins_array)
-        print('df_from_df', df_from_df)
+        # print('df_from_df', df_from_df)
 
         # merged_dataframe = pd.DataFrame(data=dictionary, columns=expanded_coins)
         # print('merged_dataframe', merged_dataframe)
 
-        print('=====================')
+        # print('=====================')
 
         data_rows = []
 
@@ -45,22 +45,29 @@ def get_crypto():
             data_rows.append(data_frame.loc[ind]['coins'])
             # print(data_frame.loc[ind]['coins'])
 
-        print('data_rows', data_rows)
+        # print('data_rows', data_rows)
 
-        custom_columns = ['name', 'price']
+        custom_columns = ['name', 'symbol', 'icon', 'price', 'priceChange1h', 'priceChange1d', 'priceChange1w']
 
-        variables = data_rows[0].keys()
-        print('variables', variables)
-        df_test = pd.DataFrame(data=data_rows, columns=custom_columns)
+        # variables = data_rows[0].keys()
+        # print('variables', variables)
+        refined_data_frame = pd.DataFrame(data=data_rows, columns=custom_columns)
 
         # renaming the DataFrame columns
-        df_test.rename(columns={'name': 'Όνομα',
-                                'price': 'Τιμή ($)'},
-                       inplace=True)
+        refined_data_frame.rename(columns={
+            'name': 'Όνομα',
+            'symbol': 'Σύμβολο',
+            'icon': 'Λογότυπο',
+            'price': 'Τιμή ($)',
+            'priceChange1h': 'Αυξομείωση 1 ώρα',
+            'priceChange1d': 'Αυξομείωση 1 ημέρα',
+            'priceChange1w': 'Αυξομείωση 1 εβδομάδα',
+        },
+            inplace=True)
 
-        print('df_test', df_test)
+        # print('refined_data_frame', refined_data_frame)
 
-        return df_test
+        return refined_data_frame
 
     except error.HTTPError as e:
         error_info = e.read().decode()
